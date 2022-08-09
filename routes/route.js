a4d3ac29c1cad42bfd358d2a7053b546ab5b034f
Kvithana1bt <kalhan.vithanage@1billiontech.com>
2022-08-09 22:50:46 +0530

    fixed

--- routes/route.js
+++ routes/route.js
@@ -13,51 +13,48 @@ router.route("/").get((req, res, next) => {
   res.send("user hello");
 });
 
-
 router.route("/login").post((req, res, next) => {
-  const { pname,password } = req.body;
- 
+  const { pname, password } = req.body;
+
   const _query = `SELECT * FROM  parent where pname = "${pname}" AND password = "${password}" ; `;
- 
-         dbConnection.db.query(_query, (err, data) => {
-           if (err) console.log(err);
-           else {
-             console.log(data)
-            res.status(200).json(data)
-           }
-         });
- 
- });
 
-
-router.route("/register").post((req, res, next) => {
- const { pname, cname ,password, comfirmPassword } = req.body;
- const  pId =Math.floor(Math.random() * 100);
- const  cId =Math.floor(Math.random() * 50);
-
- const _query = `INSERT INTO parent (pname,cname,password,comfirmPassword,pId,cId) values ("${pname}","${cname}","${password}","${comfirmPassword}","${pId}","${cId}") ; `;
-
-        dbConnection.db.query(_query, (err, data) => {
-          if (err) console.log(err);
-          else {
-            console.log("sucsss");
-            res.status(200).json("success")
-          }
-        });
-
-        
-
+  dbConnection.db.query(_query, (err, data) => {
+    if (err) console.log(err);
+    else {
+      console.log(data);
+      res.status(200).json(data);
+    }
+  });
 });
 
+router.route("/register").post((req, res, next) => {
+  const { pname, cname, password, comfirmPassword } = req.body;
+  const pId = Math.floor(Math.random() * 100);
+  const cId = Math.floor(Math.random() * 50);
+
+  const _query = `INSERT INTO parent (pname,cname,password,comfirmPassword,pId,cId) values ("${pname}","${cname}","${password}","${comfirmPassword}","${pId}","${cId}") ; `;
+
+  dbConnection.db.query(_query, (err, data) => {
+    if (err) console.log(err);
+    else {
+      console.log("sucsss");
+      res.status(200).json("success");
+    }
+  });
+});
 
 router.route("/upload").post(async (req, res, next) => {
   const { pId, cId } = req.body;
-  const filePath = `../public/images/${Date.now()}.jpg`;
+  let _fileName = `${Date.now()}.jpg`;
+  console.log(_fileName);
+  const filePath = `../public/images/${_fileName}`;
+  console.log(filePath);
   var today = new Date();
   let imgToStrng = req.body.base64.split(",")[1];
   let buffer = Buffer.from(imgToStrng, "base64");
-  const str = config.baseUrl + "/user/static/images/" + `${Date.now()}.jpg`;
-  var date =today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDate();
+  const str = config.baseUrl + "/user/static/images/" + `${_fileName}`;
+  var date =
+    today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDate();
 
   const _query = `INSERT INTO user (userImg,uploadedTime,pId,cId) values ("${str}","${date}","${pId}","${cId}") ; `;
 
@@ -119,8 +116,13 @@ router.route("/words").post(async (req, res, next) => {
         let item = _data.flat(2).splice(2, 4).join(",");
         let _item = _data.flat(2);
         var today = new Date();
-        var date =today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDate();
-       
+        var date =
+          today.getFullYear() +
+          "-" +
+          (today.getMonth() + 1) +
+          "-" +
+          today.getDate();
+
         const _query = `INSERT INTO textInput (result,pId,cId,type,date) values ("${item}","${pId}","${cId}","${_item[3]}","${date}") ; `;
 
         dbConnection.db.query(_query, (err, data) => {
@@ -140,6 +142,7 @@ router.route("/posts").post(async (req, _res, next) => {
   // let acess_token = "EAAYZB4UMZACkoBAFtZCwnZBbd2uCmT9dCOX2BolEcWZCFUNa0Tsfq9ZCgKvmaF2ayzZCt9keVnmNllLcZCXskvMHcNECC7tE4LKc1qmv5Bn30O4g6UmJ0IYLCi5a1PqZAZBzRAZAb58qvZBczPyWWySrVzMb2eCEuqbKZCuOOVdvZBIepZArEggXGOyr8OWlbwEQHzipFvRg6iUYzRDby4fsIZBZBwZCWVmp5kuExMF7YZD";
   const token = req.query;
   const { pId, cId } = req.body;
+  console.log(token.id);
   await axios
     .get(
       `https://graph.facebook.com/v14.0/me?fields=posts%7Bcomments%7D&access_token=${token.id}`
@@ -185,16 +188,23 @@ router.route("/posts").post(async (req, _res, next) => {
                 return element !== undefined;
               });
               let item = _data.flat(2).splice(2, 4).join(",");
-              let _item = _data.flat(2);
+
+              let _item = _data.flat(2).splice(1, 4);
+              console.log(_item[0]);
               var today = new Date();
-        var date =today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDate();
-       
-        const _query = `INSERT INTO posts (result,pId,cId,type,date) values ("${item}","${pId}","${cId}","${_item[2]}","${date}") ; `;
+              var date =
+                today.getFullYear() +
+                "-" +
+                (today.getMonth() + 1) +
+                "-" +
+                today.getDate();
+
+              const _query = `INSERT INTO posts (result,pId,cId,type,date) values ("${_item[0]}","${pId}","${cId}","${_item[1]}","${date}") ; `;
 
               dbConnection.db.query(_query, (err, data) => {
                 if (err) console.log(err);
                 else {
-                  console.log("sucsss");
+                  console.log("sucsss", data);
                 }
               });
             })
@@ -207,20 +217,17 @@ router.route("/posts").post(async (req, _res, next) => {
 });
 
 router.route("/words").get((req, res, next) => {
-  let type = req.query.type
+  let type = req.query.type;
   let _query;
-  if(type == 'positive'){
-  _query = `SELECT * FROM textInput where type = 'Positive' ; `;
-
-  }else{
-     _query = `SELECT * FROM textInput where type = 'Negative' ; `;
-
+  if (type == "positive") {
+    _query = `SELECT * FROM textInput where type = 'Positive' ; `;
+  } else {
+    _query = `SELECT * FROM textInput where type = 'Negative' ; `;
   }
   dbConnection.db.query(_query, (err, data) => {
     if (err) console.log(err);
     else {
       res.status(200).json(data);
-     
     }
   });
 });
@@ -238,20 +245,17 @@ router.route("/images").get((req, res, next) => {
 });
 
 router.route("/posts").get((req, res, next) => {
-  let type = req.query.type
+  let type = req.query.type;
   let _query;
-  if(type == 'no'){
-  _query = `SELECT * FROM posts where type = 'NO' ; `;
-
-  }else{
-     _query = `SELECT * FROM posts where type = 'YES' ; `;
-
+  if (type == "no") {
+    _query = `SELECT * FROM posts where type = 'NO' ; `;
+  } else {
+    _query = `SELECT * FROM posts where type ='YES'; `;
   }
   dbConnection.db.query(_query, (err, data) => {
     if (err) console.log(err);
     else {
       res.status(200).json(data);
-     
     }
   });
 });
@@ -267,6 +271,7 @@ router.route("/games").post(async (req, res, next) => {
 router.route("/insert").post(async (req, res, next) => {
   const { obj, pId, cId } = req.body;
   var today = new Date();
+  console.log(obj);
   let date =
     today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDate();
   if (obj.game === "pubg mobile") {
@@ -302,9 +307,9 @@ router.route("/insert").post(async (req, res, next) => {
 });
 
 router.route("/list").get((req, res, next) => {
-   const type = req.query.type
+  const type = req.query.type;
 
-   if(type == 'pubg'){
+  if (type == "pubg") {
     const _query = `SELECT * FROM pubg   ; `;
 
     dbConnection.db.query(_query, (err, data) => {
@@ -314,8 +319,7 @@ router.route("/list").get((req, res, next) => {
         console.log("sucsss");
       }
     });
-
-   }else{
+  } else {
     const _query = `SELECT * FROM callofduty   ; `;
 
     dbConnection.db.query(_query, (err, data) => {
@@ -325,9 +329,7 @@ router.route("/list").get((req, res, next) => {
         console.log("sucsss");
       }
     });
-
-   }
-  
+  }
 });
 
 module.exports = router;
